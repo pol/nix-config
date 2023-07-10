c0e87b23fe54b0d7f15e44892d25c611dbe5817e
Pol Llovet <pol.llovet@gmail.com>
2023-07-10 07:24:19 -0400

    typo fixed

--- flake.nix
+++ flake.nix
@@ -21,71 +21,70 @@
     darwinConfigurations.NHDQ60QHJQ = inputs.darwin.lib.darwinSystem {
       system = "aarch64-darwin";
       pkgs = import inputs.nixpkgs { system = "aarch64-darwin"; };
+      modules = [
+        ({ pkgs, ... }: {
+          # here go the darwin preferences and config items
+          programs.zsh.enable = true;
+          environment.shells = [ pkgs.bash pkgs.zsh ];
+          environment.loginShell = pkgs.zsh;
+          environment.systemPackages = [ pkgs.coreutils ];
+          nix.extraOptions = ''
+            experimental-features = nix-command flakes
+          '';
+          system.keyboard.enableKeyMapping = true;
+          system.keyboard.remapCapsLockToEscape = true;
+          fonts.fonts =
+            [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
+          services.nix-daemon.enable = true;
+          system.defaults.finder.AppleShowAllExtensions = true;
+          system.defaults.finder._FXShowPosixPathInTitle = true;
+          system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
+          system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
+          system.defaults.NSGlobalDomain.KeyRepeat = 1;
+          # backwards compat; don't change
+          system.stateVersion = 4;
+        })
+        inputs.home-manager.darwinModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.pol.imports = [
+              ({ pkgs, ... }: {
+                # Don't change this when you change package input. Leave it alone.
+                home.stateVersion = "22.11";
+                # specify my home-manager configs
+                home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
+                home.sessionVariables = {
+                  PAGER = "less";
+                  CLICLOLOR = 1;
+                  EDITOR = "nvim";
+                };
+                programs.bat.enable = true;
+                programs.bat.config.theme = "TwoDark";
+                programs.fzf.enable = true;
+                programs.fzf.enableZshIntegration = true;
+                programs.exa.enable = true;
+                programs.git.enable = true;
+                programs.zsh.enable = true;
+                programs.zsh.enableCompletion = true;
+                programs.zsh.enableAutosuggestions = true;
+                programs.zsh.enableSyntaxHighlighting = true;
+                programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
+                programs.starship.enable = true;
+                programs.starship.enableZshIntegration = true;
+                programs.alacritty = {
+                  enable = true;
+                  settings.font.normal.family = "MesloLGS Nerd Font Mono";
+                  settings.font.size = 16;
+                };
+              })
+            ];
+          };
+        }
+      ];
     };
-    modules = [
-      ({ pkgs, ... }: {
-        # here go the darwin preferences and config items
-        programs.zsh.enable = true;
-        environment.shells = [ pkgs.bash pkgs.zsh ];
-        environment.loginShell = pkgs.zsh;
-        environment.systemPackages = [ pkgs.coreutils ];
-        nix.extraOptions = ''
-          experimental-features = nix-command flakes
-        '';
-        system.keyboard.enableKeyMapping = true;
-        system.keyboard.remapCapsLockToEscape = true;
-        fonts.fonts =
-          [ (pkgs.nerdfonts.override { fonts = [ "Meslo" ]; }) ];
-        services.nix-daemon.enable = true;
-        system.defaults.finder.AppleShowAllExtensions = true;
-        system.defaults.finder._FXShowPosixPathInTitle = true;
-        system.defaults.NSGlobalDomain.AppleShowAllExtensions = true;
-        system.defaults.NSGlobalDomain.InitialKeyRepeat = 14;
-        system.defaults.NSGlobalDomain.KeyRepeat = 1;
-        # backwards compat; don't change
-        system.stateVersion = 4;
-      })
-      inputs.home-manager.darwinModules.home-manager {
-        home-manager = {
-          useGlobalPkgs = true;
-          useUserPackages = true;
-          users.pol.imports = [
-            ({ pkgs, ... }: {
-              # Don't change this when you change package input. Leave it alone.
-              home.stateVersion = "22.11";
-              # specify my home-manager configs
-              home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
-              home.sessionVariables = {
-                PAGER = "less";
-                CLICLOLOR = 1;
-                EDITOR = "nvim";
-              };
-              programs.bat.enable = true;
-              programs.bat.config.theme = "TwoDark";
-              programs.fzf.enable = true;
-              programs.fzf.enableZshIntegration = true;
-              programs.exa.enable = true;
-              programs.git.enable = true;
-              programs.zsh.enable = true;
-              programs.zsh.enableCompletion = true;
-              programs.zsh.enableAutosuggestions = true;
-              programs.zsh.enableSyntaxHighlighting = true;
-              programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
-              programs.starship.enable = true;
-              programs.starship.enableZshIntegration = true;
-              programs.alacritty = {
-                enable = true;
-                settings.font.normal.family = "MesloLGS Nerd Font Mono";
-                settings.font.size = 16;
-              };
-            })
-          ];
-        };
-      }
-    ];
   };
 }
-
 
 #   outputs = { self, darwin, nixpkgs, home-manager, ... }@inputs:
 #   let 

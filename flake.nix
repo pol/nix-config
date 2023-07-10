aa6a2e984ea9c5e84226c9c46754d89995acbee0
Pol Llovet <pol.llovet@gmail.com>
2023-07-10 08:46:15 -0400

    move home-manager config to home.nix

--- flake.nix
+++ flake.nix
@@ -48,42 +48,13 @@
             system.defaults.NSGlobalDomain.KeyRepeat = 1;
             # backwards compat; don't change
             system.stateVersion = 4;
+            users.users.pol = { home = "/Users/pol"; };
           })
           inputs.home-manager.darwinModules.home-manager {
             home-manager = {
               useGlobalPkgs = true;
               useUserPackages = true;
-              users.pol.imports = [
-                ({ pkgs, ... }: {
-                  # Don't change this when you change package input. Leave it alone.
-                  home.stateVersion = "22.11";
-                  # specify my home-manager configs
-                  home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
-                  home.sessionVariables = {
-                    PAGER = "less";
-                    CLICLOLOR = 1;
-                    EDITOR = "nvim";
-                  };
-                  programs.bat.enable = true;
-                  programs.bat.config.theme = "TwoDark";
-                  programs.fzf.enable = true;
-                  programs.fzf.enableZshIntegration = true;
-                  programs.exa.enable = true;
-                  programs.git.enable = true;
-                  programs.zsh.enable = true;
-                  programs.zsh.enableCompletion = true;
-                  programs.zsh.enableAutosuggestions = true;
-                  programs.zsh.syntaxHighlighting.enable = true;
-                  programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
-                  programs.starship.enable = true;
-                  programs.starship.enableZshIntegration = true;
-                  programs.alacritty = {
-                    enable = true;
-                    settings.font.normal.family = "MesloLGS Nerd Font Mono";
-                    settings.font.size = 16;
-                  };
-                })
-              ];
+              users.pol = import ./home.nix;
             };
           }
         ];
--- home.nix
+++ home.nix
@@ -1,7 +1,34 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.stateVersion = "22.05";
+  home.stateVersion = "23.11";
+  # Don't change this when you change package input. Leave it alone.
+  home.stateVersion = "23.11";
+  # specify my home-manager configs
+  home.packages = [ pkgs.ripgrep pkgs.fd pkgs.curl pkgs.less ];
+  home.sessionVariables = {
+    PAGER = "less";
+    CLICLOLOR = 1;
+    EDITOR = "nvim";
+  };
+  programs.bat.enable = true;
+  programs.bat.config.theme = "TwoDark";
+  programs.fzf.enable = true;
+  programs.fzf.enableZshIntegration = true;
+  programs.exa.enable = true;
+  programs.git.enable = true;
+  programs.zsh.enable = true;
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.syntaxHighlighting.enable = true;
+  programs.zsh.shellAliases = { ls = "ls --color=auto -F"; };
+  programs.starship.enable = true;
+  programs.starship.enableZshIntegration = true;
+  programs.alacritty = {
+    enable = true;
+    settings.font.normal.family = "MesloLGS Nerd Font Mono";
+    settings.font.size = 16;
+  };
 
   # https://github.com/malob/nixpkgs/blob/master/home/default.nix
 
